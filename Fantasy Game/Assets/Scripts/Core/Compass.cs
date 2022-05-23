7cffdb17d33b4fe8a4aca1580e662a98b0e90364
LightPat <60397186+LightPat@users.noreply.github.com>
2022-05-23 17:02:28 -0400

    GUI Compass

---
+++ Fantasy Game/Assets/Scripts/Core/Compass.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LightPat
+{
+    public class Compass : MonoBehaviour
+    {
+        public GUIStyle style = new GUIStyle();
+
+        private void OnGUI()
+        {
+            Vector3 point = Camera.main.transform.forward;
+
+            if (point.x >= 0)
+            {
+                if (point.z >= 0)
+                {
+                    GUI.Label(new Rect(10, 10, 100, 20), "NorthEast", style);
+                }
+                else // z < 0
+                {
+                    GUI.Label(new Rect(10, 10, 100, 20), "SouthEast", style);
+                }
+            }
+            else // x < 0
+            {
+                if (point.z >= 0)
+                {
+                    GUI.Label(new Rect(10, 10, 100, 20), "NorthWest", style);
+                }
+                else // z < 0
+                {
+                    GUI.Label(new Rect(10, 10, 100, 20), "SouthWest", style);
+                }
+            }
+        }
+    }
+}

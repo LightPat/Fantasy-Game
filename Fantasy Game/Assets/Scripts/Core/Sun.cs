4d5641fd97894eb1f890dda1857f54a97d0904b2
LightPat <60397186+LightPat@users.noreply.github.com>
2022-05-23 15:34:52 -0400

    Basic Day Night Cycle (Rotating Sun)

--- Fantasy Game/Assets/Scripts/Core/Sun.cs
+++ Fantasy Game/Assets/Scripts/Core/Sun.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 namespace LightPat.Core
 {
-    public class Rotate : MonoBehaviour
+    public class Sun : MonoBehaviour
     {
+        public float cycleSpeed;
+
         void Update()
         {
-            transform.Rotate(0.1f, 0.3f, 0.5f);
+            transform.Rotate(0, cycleSpeed, 0);
         }
     }
 }

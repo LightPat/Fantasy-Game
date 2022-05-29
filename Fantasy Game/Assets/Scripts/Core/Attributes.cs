09be1c1556e147c64b4e7d354848774ad62d446c
LightPat <60397186+LightPat@users.noreply.github.com>
2022-05-29 19:19:30 -0400

    Display Health in text

--- Fantasy Game/Assets/Scripts/Core/Attributes.cs
+++ Fantasy Game/Assets/Scripts/Core/Attributes.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace LightPat.Core
 {
     public class Attributes : MonoBehaviour
     {
         public float maxHealth = 100f;
+        public TextMeshProUGUI overlayText;
+        public TextMeshPro worldSpaceText;
         private float HP;
 
         private void Start()
         {
             HP = maxHealth;
+            SetDisplayText(HP.ToString() + " HP");
         }
 
         public void InflictDamage(float damage, GameObject inflicter)
         {
             HP -= damage;
+            SetDisplayText(HP.ToString() + " HP");
             SendMessage("OnAttacked", inflicter);
             if (HP <= 0)
             {
@@ -27,6 +32,18 @@ namespace LightPat.Core
         public float GetHP()
         {
             return HP;
+        }
+
+        private void SetDisplayText(string newText)
+        {
+            if (overlayText != null)
+            {
+                overlayText.SetText(newText);
+            }
+            if (worldSpaceText != null)
+            {
+                worldSpaceText.SetText(newText);
+            }
         }
     }
 }

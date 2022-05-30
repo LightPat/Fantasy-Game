a46c3b89a0576b919edc8f288f050203c85b3762
LightPat <60397186+LightPat@users.noreply.github.com>
2022-05-30 14:47:20 -0400

    Info label scale animation

--- Fantasy Game/Assets/Scripts/UI/WorldSpaceInfoLabel.cs
+++ Fantasy Game/Assets/Scripts/UI/WorldSpaceInfoLabel.cs
@@ -7,12 +7,23 @@ namespace LightPat.UI
     public class WorldSpaceInfoLabel : MonoBehaviour
     {
         public float rotationSpeed;
-        public float viewDistance;
+        public float animationSpeed = 0.02f;
+        public float viewDistance = 10f;
 
         private void Update()
         {
             transform.LookAt(Camera.main.transform);
             transform.Rotate(0, 180, 0);
+
+            if (Vector3.Distance(Camera.main.transform.position, transform.position) > viewDistance)
+            {
+                // Might have to add time.DeltaTime to this instead of just using animationSpeed
+                transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, animationSpeed);
+            }
+            else
+            {
+                transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one, animationSpeed);
+            }
         }
     }
 }

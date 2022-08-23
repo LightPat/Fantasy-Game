62b0f7d609ab25d308c3ab0b93026dbcbe40dd89
LightPat <60397186+LightPat@users.noreply.github.com>
2022-08-22 22:53:34 -0400

    Deleted Old player controller

--- Fantasy Game/Assets/Scripts/EnemyAI/RoamingMonster.cs
+++ Fantasy Game/Assets/Scripts/EnemyAI/RoamingMonster.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using LightPat.Core;
-using UnityEditor;
+using LightPat.Core.Player;
 
 namespace LightPat.EnemyAI
 {

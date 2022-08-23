e0822185d264a4874413dfc31325fc7439b98f3c
LightPat <60397186+LightPat@users.noreply.github.com>
2022-08-23 14:02:18 -0400

    Camera Return Tracking Euler Angles Fixed

--- Fantasy Game/Assets/Scripts/Core/Player/PlayerCameraFollow.cs
+++ Fantasy Game/Assets/Scripts/Core/Player/PlayerCameraFollow.cs
@@ -18,7 +18,15 @@ namespace LightPat.Core.Player
             if (UpdateRotationWithTarget)
             {
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, rotationSpeed * Time.deltaTime);
-                player.rotationX = 360 - transform.eulerAngles.x;
+
+                if (transform.eulerAngles.x > player.mouseDownXRotLimit) // If our number is greater than the positive look bound, make it a negative angle
+                {
+                    player.rotationX = 360 - transform.eulerAngles.x;
+                }
+                else
+                {
+                    player.rotationX = transform.eulerAngles.x;
+                }
                 player.rotationY = transform.eulerAngles.y;
             }
             else

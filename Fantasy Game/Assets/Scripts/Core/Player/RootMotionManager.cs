98e85b6e4397b1ebd410038eae6326b0eaaf38d1
LightPat <60397186+LightPat@users.noreply.github.com>
2022-09-10 19:19:53 -0400

    Better slow motion logic

--- Fantasy Game/Assets/Scripts/Core/Player/RootMotionManager.cs
+++ Fantasy Game/Assets/Scripts/Core/Player/RootMotionManager.cs
@@ -17,19 +17,9 @@ namespace LightPat.Core.Player
         public float drag;
         private void OnAnimatorMove()
         {
-            Vector3 newVelocity;
-            if (drag == 1)
-            {
-                newVelocity = Vector3.MoveTowards(rb.velocity * Time.timeScale, animator.velocity, drag);
-                newVelocity.y = rb.velocity.y * Time.timeScale;
-                rb.velocity = newVelocity / Time.timeScale;
-            }                
-            else
-            {
-                newVelocity = Vector3.MoveTowards(rb.velocity, animator.velocity, drag);
-                newVelocity.y = rb.velocity.y;
-                rb.velocity = newVelocity;
-            }
+            Vector3 newVelocity = Vector3.MoveTowards(rb.velocity * Time.timeScale, animator.velocity, drag);
+            newVelocity.y = rb.velocity.y * Time.timeScale;
+            rb.velocity = newVelocity / Time.timeScale;
         }
 
         //private void OnAnimatorMove()

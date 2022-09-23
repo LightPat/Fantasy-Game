f111235ed9171bbbe992a51549e7a2deb5b7864c
LightPat <60397186+LightPat@users.noreply.github.com>
2022-09-23 11:17:32 -0400

    Pistol Position Offset using Shoulders

--- Fantasy Game/Assets/Scripts/Util/PistolPositionSolver.cs
+++ Fantasy Game/Assets/Scripts/Util/PistolPositionSolver.cs
@@ -10,12 +10,15 @@ namespace LightPat.Util
         public float forwardMult;
         public float rightMult;
         public float upMult;
+        public Transform firstShoulder;
+        public Transform secondShoulder;
 
         private void Update()
         {
             transform.position = parentBone.position;
             transform.rotation = parentBone.rotation;
             transform.position += transform.forward * forwardMult + transform.right * rightMult + transform.up * upMult;
+            transform.position += firstShoulder.position - secondShoulder.position;
         }
     }
 }

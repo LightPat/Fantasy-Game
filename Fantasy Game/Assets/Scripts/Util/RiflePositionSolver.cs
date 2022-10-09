7ff9727e2c604b27dc56a8097b464f735de6f57a
LightPat <60397186+LightPat@users.noreply.github.com>
2022-10-09 03:55:25 -0400

    New rifle grip

---
+++ Fantasy Game/Assets/Scripts/Util/RiflePositionSolver.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LightPat.Util
+{
+    public class RiflePositionSolver : MonoBehaviour
+    {
+        public Transform parentBone;
+        public Vector3 positionMults;
+        public Transform firstShoulder;
+        public Transform secondShoulder;
+
+        public void UpdateMultipliers(Vector3 newMultipliers)
+        {
+            positionMults = newMultipliers;
+        }
+
+        private void Update()
+        {
+            transform.rotation = parentBone.rotation;
+            Vector3 endPosition = parentBone.position;
+            endPosition += transform.right * positionMults.x + transform.up * positionMults.y + transform.forward * positionMults.z;
+            //endPosition += firstShoulder.position - secondShoulder.position;
+            transform.position = endPosition;
+        }
+    }
+}

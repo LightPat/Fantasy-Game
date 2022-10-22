cd34c4bbac11557795b9e55ea2dc10acf30ed90e
LightPat <60397186+LightPat@users.noreply.github.com>
2022-10-22 13:38:46 -0400

    Great Sword swinging bool activates after 1/5 of the swing animation has completed

--- Fantasy Game/Assets/Scripts/Core/Player/WeaponSystem/GreatSword.cs
+++ Fantasy Game/Assets/Scripts/Core/Player/WeaponSystem/GreatSword.cs
@@ -11,9 +11,24 @@ namespace LightPat.Core.Player
         public float swingSpeed = 1;
         public bool swinging { get; private set; }
 
+        Animator playerAnimator;
+        Coroutine swingRoutine;
+
         public override void Attack1(bool pressed)
         {
-            swinging = pressed;
+            if (swingRoutine != null)
+                StopCoroutine(swingRoutine);
+            if (pressed)
+                swingRoutine = StartCoroutine(Swing());
+            else
+                swinging = false;
+        }
+
+        private IEnumerator Swing()
+        {
+            yield return new WaitUntil(() => playerAnimator.IsInTransition(playerAnimator.GetLayerIndex("Great Sword")));
+            yield return new WaitForSeconds(playerAnimator.GetNextAnimatorClipInfo(playerAnimator.GetLayerIndex("Great Sword"))[0].clip.length * 0.2f);
+            swinging = true;
         }
 
         private new void Start()
@@ -25,7 +40,10 @@ namespace LightPat.Core.Player
         private void OnTransformParentChanged()
         {
             if (GetComponentInParent<HumanoidWeaponAnimationHandler>())
-                GetComponentInParent<Animator>().SetFloat("swingSpeed", swingSpeed);
+            {
+                playerAnimator = GetComponentInParent<Animator>();
+                playerAnimator.SetFloat("swingSpeed", swingSpeed);
+            }
         }
     }
 }

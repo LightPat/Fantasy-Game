53c6405ab555cea37c569e36bb85901d1566dc93
LightPat <60397186+LightPat@users.noreply.github.com>
2023-01-13 07:40:41 -0500

    Server FPS Counter

--- Fantasy Game/Assets/Scripts/Core/Player/ServerCamera.cs
+++ Fantasy Game/Assets/Scripts/Core/Player/ServerCamera.cs
@@ -9,6 +9,7 @@ namespace LightPat.Core.Player
     {
         public float moveSpeed = 1;
         public float sensitivity = 0.1f;
+        int fps;
 
         private void Start()
         {
@@ -20,6 +21,16 @@ namespace LightPat.Core.Player
             if (pauseEnabled) { return; }
 
             transform.Translate(new Vector3(moveInput.x, 0, moveInput.y) * moveSpeed);
+            fps = Mathf.RoundToInt((float)1.0 / Time.deltaTime);
+        }
+
+        private void OnGUI()
+        {
+            // FPS Label
+            GUIStyle guiStyle = new GUIStyle();
+            guiStyle.fontSize = 48;
+            guiStyle.normal.textColor = Color.yellow;
+            GUI.Label(new Rect(Screen.currentResolution.width - 100, 50, 100, 50), fps.ToString(), guiStyle);
         }
 
         Vector2 moveInput;
